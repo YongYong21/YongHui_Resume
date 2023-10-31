8e2f508480ceff23993c77f3f61f9aaafbd2cc56
YongYong21 <imkinggosu@gmail.com>
2023-10-31 22:53:23 +0900

    Feat:Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
+/** @jsxImportSource @emotion/react */
 import { useState } from "react";
-
 import "./App.css";
+import { Global, css } from "@emotion/react";
+import { globalDarkStyles, globalLightStyles } from "./theme";
+
+import HeaderCS from "./components/HeaderCS";
+import { useEffect } from "react";
 
 function App(): JSX.Element {
-  return <>gg</>;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => {
+      localStorage.setItem("hasDarkModeState", JSON.stringify(!prevMode));
+      return !prevMode;
+    });
+  };
+  useEffect(() => {
+    const hasDarkModeState = localStorage.getItem("hasDarkModeState");
+    if (hasDarkModeState) {
+      return JSON.parse(hasDarkModeState) === false
+        ? setIsDarkMode(false)
+        : setIsDarkMode(true);
+    } else {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        setIsDarkMode(false);
+        localStorage.setItem("hasDarkModeState", JSON.stringify(isDarkMode));
+      } else {
+        setIsDarkMode(true);
+        localStorage.setItem("hasDarkModeState", JSON.stringify(isDarkMode));
+      }
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <button onClick={toggleTheme}>Toggle Theme</button>
+
+      <Global styles={isDarkMode ? globalLightStyles : globalDarkStyles} />
+      <HeaderCS />
+    </div>
+  );
 }
 
 export default App;
--- src/theme.ts
+++ src/theme.ts
@@ -4,11 +4,27 @@ export const globalLightStyles = css`
   body {
     font-family: "Arial", sans-serif;
     background-color: #f8f9fb;
+    color: #171316;
   }
 `;
 export const globalDarkStyles = css`
   body {
     font-family: "Arial", sans-serif;
     background-color: #171316;
+    color: #f8f9fb;
   }
 `;
+export const fontSizes = {
+  xsm: "10px",
+  sm: "12px",
+  md: "16px",
+  lg: "20px",
+  xl: "24px",
+  xxl: "28px"
+};
+export const fontWeights = {
+  extraBold: 800,
+  bold: 700,
+  semiBold: 600,
+  regular: 400
+};
